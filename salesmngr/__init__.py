852a0eb5cc508fa3b4c370a6a872f774e0cb3c8b
valtterisyvaniemi <valtteri@syvaniemi.com>
2023-04-24 11:53:07 +0300

    second commit

--- salesmngr/__init__.py
+++ salesmngr/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -6,8 +7,8 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "m16y secret key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DB_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,8 +18,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "mngr.integraatio@gmail.com"
-app.config['MAIL_PASSWORD'] = "nzkgzpbxglazlrqg"
+app.config['MAIL_USERNAME'] = os.environ.get("MAIL_ADDRESS")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
 mail = Mail(app)
 
 from salesmngr import routes
