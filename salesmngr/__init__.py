5f3217c39618021008f63cda1e128b78a8cdc6ca
valtterisyvaniemi <valtteri@syvaniemi.com>
2023-04-28 15:18:14 +0300

    28.4. sql uri5 commit

--- salesmngr/__init__.py
+++ salesmngr/__init__.py
@@ -8,7 +8,9 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
