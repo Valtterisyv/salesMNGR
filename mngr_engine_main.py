e35c928cc9d0892ae6e7e2335f567b37322bd98f
valtterisyvaniemi <valtteri@syvaniemi.com>
2023-05-09 10:10:40 +0300

    good days testing commit

--- mngr_engine_main.py
+++ mngr_engine_main.py
@@ -842,56 +842,57 @@ for name in USER_NAME_LIST:
                 if user:
                     today = str(user.today)
                     date_today = str(date.today())
-                    if weekday_now != "Sat" or weekday_now != "Sun":
-                        good_days = user.good_days
-                        bad_days = user.bad_days
-                        if my_activities_today > person_1.required_daily_calls:
-                            if good_days + bad_days < 21:
-                                good_days += 1
-                            else:
-                                good_days += 1
-                                bad_days -= 1
-                        elif 0 < my_activities_today < person_1.required_daily_calls:
-                            if good_days + bad_days < 21:
-                                bad_days += 1
-                            else:
-                                bad_days += 1
-                                good_days -= 1
-                        if good_days > 0:
-                            good_percent = round(good_days * 100 / (good_days + bad_days))
-                        else:
-                            good_percent = 0
+                    if today != date_today:
+                        if weekday_now != "Sat" or weekday_now != "Sun":
+                            if time_now > "23:00":
+                                good_days = user.good_days
+                                bad_days = user.bad_days
+                                if my_activities_today > person_1.required_daily_calls:
+                                    if good_days + bad_days < 21:
+                                        good_days += 1
+                                    else:
+                                        good_days += 1
+                                        bad_days -= 1
+                                elif 0 < my_activities_today < person_1.required_daily_calls:
+                                    if good_days + bad_days < 21:
+                                        bad_days += 1
+                                    else:
+                                        bad_days += 1
+                                        good_days -= 1
+                                if good_days > 0:
+                                    good_percent = round(good_days * 100 / (good_days + bad_days))
+                                else:
+                                    good_percent = 0
 
-                        user.good_days = good_days
-                        user.bad_days = bad_days
-                        user.good_percent = good_percent
-                        user.today = date_today
+                                user.good_days = good_days
+                                user.bad_days = bad_days
+                                user.good_percent = good_percent
+                                user.today = date_today
 
-                        db.session.commit()
-                        print(f"{user_name} - good_upgrade success!")
-                        print(f"{date_today}: {good_days} / {bad_days} = {good_percent}")
-                        print(time_now)
+                                db.session.commit()
+                                print(f"{user_name} - good_upgrade success!")
                 else:
                     if weekday_now != "Sat" or weekday_now != "Sun":
-                        good_days = 0
-                        bad_days = 0
-                        if my_activities_today > person_1.required_daily_calls:
-                            if good_days + bad_days < 21:
-                                good_days += 1
+                        if time_now > "23:00":
+                            good_days = 0
+                            bad_days = 0
+                            if my_activities_today > person_1.required_daily_calls:
+                                if good_days + bad_days < 21:
+                                    good_days += 1
+                                else:
+                                    good_days += 1
+                                    bad_days -= 1
+                            elif 0 < my_activities_today < person_1.required_daily_calls:
+                                if good_days + bad_days < 21:
+                                    bad_days += 1
+                                else:
+                                    bad_days += 1
+                                    good_days -= 1
+                            if good_days > 0:
+                                good_percent = round(good_days * 100 / (good_days + bad_days))
                             else:
-                                good_days += 1
-                                bad_days -= 1
-                        elif 0 < my_activities_today < person_1.required_daily_calls:
-                            if good_days + bad_days < 21:
-                                bad_days += 1
-                            else:
-                                bad_days += 1
-                                good_days -= 1
-                        if good_days > 0:
-                            good_percent = round(good_days * 100 / (good_days + bad_days))
-                        else:
-                            good_percent = 0
-                        date_today = str(date.today())
+                                good_percent = 0
+                            date_today = str(date.today())
 
             with app.app_context():
                 user = UserData.query.filter_by(user=user_name).first()
