6da92e47fbdffcebb80d2e691a51c2811b9cd7dc
valtterisyvaniemi <valtteri@syvaniemi.com>
2023-05-16 10:15:22 +0300

    front data update commit

--- salesmngr/models.py
+++ salesmngr/models.py
@@ -10,7 +10,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_and_lastname = db.Column(db.String(50), unique=True, nullable=False)
+    first_and_lastname = db.Column(db.String(50), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
